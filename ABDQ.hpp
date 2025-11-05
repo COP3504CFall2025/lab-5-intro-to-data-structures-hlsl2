f75d289feb4b2a3b30b589a701cc9dbbd76ee9e6
lucas-hlsl <lucas@blueberry.localdomain>
2025-11-05 13:44:25 -0500

    hotfix

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -98,6 +98,7 @@ public:
         T val = arr[0];
         --size;
         for (size_t i = 0; i < size; ++i) {
+            std::cout << i << std::endl;
             arr[i] = arr[i + 1];
         }
         return val;
