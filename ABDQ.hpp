2a9e9ace47d78ec08ff9f34e4f78921ca8974656
lucas-hlsl <lucas@blueberry.localdomain>
2025-11-04 23:33:33 -0500

    hotfix

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -27,7 +27,7 @@ private:
 public:
     // Big 5
     ABDQ() : capacity(1), size(0), front_(0), back_(0), arr(new T[1]) {}
-    explicit ABDQ(std::size_t c) : capacity(c), size(0), front_(0), back_(0), arr(new T[1]) {}
+    explicit ABDQ(std::size_t c) : capacity(c), size(0), front_(0), back_(0), arr(new T[c]) {}
     ABDQ(const ABDQ& other) {
         capacity = other.capacity;
         size = 0;
